a5cc8e004fb30f8a134ec8adff955f849e0801b0
Mohamed Lazaar <lazzarmohamed10@gmail.com>
2025-11-30 09:00:05 +0100

    Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self, account_balance):
+    def __init__(self, account_balance=0):
         self._account_balance = account_balance
     def deposit(self, amount):
         self._account_balance += amount
